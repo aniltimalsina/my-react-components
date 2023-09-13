39a0261160a0ab228e4baed47bcf18b27b6c4f70
aniltimalsina <timalsinaanil96@gmail.com>
2023-09-13 13:15:53 -0400

    add components in srx/components folder

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/header";
+import Navigation from "./components/navigation";
+import Home from "./components/home";
+import About from "./components/about";
+import Contact from "./components/contact";
+
+function App() {
+  return (
+    <>
+      <div>My React components</div>
+      <Header />
+      <Navigation />
+      <Home />
+      <About />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
